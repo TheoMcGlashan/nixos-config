9bd20b419421dcdc72bc91d55d3a2523eba00b2c
TheoMcGlashan <theomcglashan@proton.me>
2026-07-19 03:53:01 -0400

    added and configured yazi

--- home.nix
+++ home.nix
@@ -18,6 +18,35 @@
 		enableZshIntegration = true;
 	};
 
+	programs.yazi = {
+		enable = true;
+		enableZshIntegration = true;
+
+		settings = {
+			mgr = {
+				ration = [ 1 3 4 ];
+				sort_by = "alphabetical";
+				sort_dir_first = true;
+				show_hidden = false;
+			};
+
+			preview = {
+				max_width = 1000;
+				max_height = 1000;
+			};
+		};
+
+		keymap = {
+			mgr.prepend_keymap = [
+				{
+					on = [ "g" "h" ];
+					run = "cd ~";
+					desc = "Go home";
+				}
+			];
+		};
+	};
+
 	programs.zoxide = {
 		enable = true;
 		enableZshIntegration = true;
@@ -152,6 +181,12 @@
 
 		texliveMedium
 		zathura
+
+		ffmpegthumbnailer
+		poppler-utils
+		imagemagick
+		jq
+		ueberzugpp
 	];
 
 	xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
