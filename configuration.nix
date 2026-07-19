c32f366c9941be5e01dc7e343ad5eb50669b39d8
TheoMcGlashan <theomcglashan@proton.me>
2026-07-19 17:07:29 -0400

    continuing to clean up structure to move to pc

--- configuration.nix
+++ configuration.nix
@@ -6,31 +6,29 @@
       ./niri.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot = {
-		enable = true;
-		configurationLimit = 10;
-	};
-  boot.loader.efi.canTouchEfiVariables = true;
-
+	# Define the name of the computer.
   networking.hostName = "nixos";
 
-	hardware.bluetooth = {
-		enable = true;
-		powerOnBoot = true;
-	};
+  # Define a user account.
+  users.users."thoe" = {
+    isNormalUser = true;
+    description = "Theo McGlashan";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
 
-	services.blueman.enable = true;
+	# Define the keyboard layout.
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-  # Set your time zone.
+	# Set timezone and locales
   time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -43,11 +41,35 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+	# Bluetooth support, GUI and applet.
+	services.blueman.enable = true;
+	hardware.bluetooth = {
+		enable = true;
+		powerOnBoot = true;
+	};
+
+	# Battery management.
+	services.tlp.enable = true;
+	services.power-profiles-daemon.enable = false;
+
+	# Services for printing support.
+	services.printing.enable = true;
+	services.avahi = {
+		enable = true;
+		nssmdns4 = true;
+	};
+
+	# Allow firmware updates.
+	services.fwupd.enable = true;
+
+	# Lock and suspend computer when lid closed.
+	services.logind.settings.Login = {
+		HandleLidSwitch = "suspend";
+		HandleLidSwitchExternalPower = "suspend";
+	};
+
+	# Enable wifi through network manager.
+  networking.networkmanager.enable = true;
 
 	# Configure keyd system-wide to remap keys
 	services.keyd = {
@@ -77,47 +99,16 @@
 		};
 	};
 
-	services.tlp.enable = true;
-	services.printing.enable = true;
-	services.avahi = {
-		enable = true;
-		nssmdns4 = true;
-	};
-	services.fwupd.enable = true;
-
-	services.power-profiles-daemon.enable = false;
-
-	services.logind.settings.Login = {
-		HandleLidSwitch = "suspend";
-		HandleLidSwitchExternalPower = "suspend";
-	};
-
-  # Define a user account.
-  users.users."thoe" = {
-    isNormalUser = true;
-    description = "Theo McGlashan";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-	# Keep vim around system-wide, just in case
-  environment.systemPackages = with pkgs; [
-    vim
-	];
-
 	# Enable zsh and make it default
 	programs.zsh.enable = true;
 	users.users.thoe.shell = pkgs.zsh;
 
+	# Enable steam, with necessary configurations.
 	programs.steam = {
 		enable = true;
 		remotePlay.openFirewall = true;
 		dedicatedServer.openFirewall = false;
 	};
-
 	hardware.graphics = {
 		enable = true;
 		enable32Bit = true;
@@ -126,7 +117,7 @@
 	# Enable flakes, and some flake commands, like update
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-	# Automatically remove old system versions
+	# Automatically remove old generations.
 	nix.gc = {
 		automatic = true;
 		dates = "weekly";
@@ -135,6 +126,12 @@
 	# Optimize stored system versions
 	nix.optimise.automatic = true;
 	nix.optimise.dates = [ "weekly" ];
+	# Cap the number of generations shown in bootloader to 10.
+  boot.loader.systemd-boot = {
+		enable = true;
+		configurationLimit = 10;
+	};
+  boot.loader.efi.canTouchEfiVariables = true;
 
 	# Nixos system version. Don't mess with this.
   system.stateVersion = "26.05";
