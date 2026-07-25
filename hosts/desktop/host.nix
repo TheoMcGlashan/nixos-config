0ea0d420432f677d446355b080c2bd04e91e56a5
TheoMcGlashan <47095401+TheoMcGlashan@users.noreply.github.com>
2026-07-25 18:48:16 -0400

    changed llm service to only run on desktop

--- hosts/desktop/host.nix
+++ hosts/desktop/host.nix
@@ -3,4 +3,10 @@
   networking.hostName = "desktop";
 
   hardware.graphics.enable32Bit = true;
+
+	# Enable ollama for local llms.
+	services.ollama = {
+		enable = true;
+		package = pkgs.ollama-rocm;
+	};
 }
