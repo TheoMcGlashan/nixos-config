3e33dbed61dd136bf5e9f9a6f3c4d31fd41bdd72
TheoMcGlashan <47095401+TheoMcGlashan@users.noreply.github.com>
2026-07-25 18:33:46 -0400

    Revert "feat: Add ProtonVPN and Vesktop configuration to NixOS desktop setup"
    This reverts commit c5c49b74a6b073921244c16f5fb7a6d3dae182ca.

--- hosts/desktop/host.nix
+++ hosts/desktop/host.nix
@@ -3,28 +3,4 @@
   networking.hostName = "desktop";
 
   hardware.graphics.enable32Bit = true;
-
-  services.protonvpn = {
-    enable = true;
-    username = "your-protonvpn-username";
-    passwordFile = "/etc/protonvpn/password";
-  };
-
-  environment.systemPackages = with pkgs; [
-    vdesktop
-    protonvpn-cli
-  ];
-
-  systemd.services.vdesktop = {
-    enable = true;
-    description = "Vesktop Service";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.vdesktop}/bin/vdesktop";
-      Restart = "always";
-    };
-  };
-
-  networking.networkmanager.unmanagedDevices = [ "eth0" ]; # Adjust the device name as necessary
 }
