dd33941ee9f86faa459b99c071acea6436bb466e
TheoMcGlashan <47095401+TheoMcGlashan@users.noreply.github.com>
2026-08-03 10:15:09 -0700

    changed keyd so that esc key and ctrl key retain their functionality, alongside capslock working as them

--- imports/keyd.nix
+++ imports/keyd.nix
@@ -7,16 +7,7 @@
 			ids = [ "*" ];
 			settings = {
 				main = {
-					shift 		= "oneshot(shift)";
-					meta 			= "oneshot(meta)";
-					control 	= "oneshot(control)";
-
-					leftalt 	= "oneshot(alt)";
-					rightalt 	= "oneshot(altgr)";
-
 					capslock 	= "overload(control, esc)";
-					esc				= "capslock";
-					insert		= "S-insert";
 				};
 				alt = {
 					h = "left";
