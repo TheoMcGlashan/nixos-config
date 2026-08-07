b20cf0e3a1d2174f208946cb02853504c3b2d799
TheoMcGlashan <47095401+TheoMcGlashan@users.noreply.github.com>
2026-08-07 11:21:36 -0700

    trying to get latex working propperly

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -19,9 +19,11 @@ vim.cmd("colorscheme tokyonight-night")
 
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_view_zathura_options = '-x "GDK_BACKEND=x11 zathura --synctex-editor-command"'
 vim.g.vimtex_compiler_latexmk = {
     options = {
-				"-c",
+				"-pdf",
+				"-auxdir=build",
         '-interaction=nonstopmode',
         '-synctex=1',
     },
