4b50ab20c28a70b5e7f34fcb8c03e395af0a1012
TheoMcGlashan <47095401+TheoMcGlashan@users.noreply.github.com>
2026-08-18 15:07:33 -0700

    made new ssd auto mount for desktop only

--- hosts/desktop/host.nix
+++ hosts/desktop/host.nix
@@ -1,8 +1,14 @@
-{ pkgs, ... }:
+{ configs, pkgs, ... }:
 {
   networking.hostName = "desktop";
 
   hardware.graphics.enable32Bit = true;
+
+	fileSystems."/disk2" = {
+		device = "/dev/disk/by-uuid/4b0f208f-901c-47c1-b3eb-ae1920b28748";
+		fsType = "ext4";
+		options = [ "defaults" "noatime" ];
+	};
 
 	# Enable ollama for local llms.
 	services.ollama = {
